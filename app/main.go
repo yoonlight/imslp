6f0a0e9ecd8c3c313924fe96783c77ebfa5b0637
kim jinwook <kjw@f1security.co.kr>
2020-07-21 17:50:08 +0900

    change gui platform

--- app/main.go
+++ app/main.go
@@ -1,162 +1,82 @@
 package main
 
 import (
-	"fmt"
+	"imslp/search"
+	"log"
 
-	"github.com/andlabs/ui"
-	_ "github.com/andlabs/ui/winmanifest"
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
+
+	//"fyne.io/fyne/theme"
+	"fyne.io/fyne/widget"
 )
 
 func main() {
-	err := ui.Main(func() {
+	app := app.New() //app 선언
+	//app.Settings().SetTheme(theme.LightTheme()) //밝은 테마 설정
 
-		// var title []string
-		mh := newModelHandler()
-
-		model := ui.NewTableModel(mh)
-		table := ui.NewTable(&ui.TableParams{
-			Model:                         model,
-			RowBackgroundColorModelColumn: -1,
-		})
-		table.AppendTextColumn("Title",
-			0, ui.TableModelColumnNeverEditable, nil)
-		// table.AppendTextColumn("Editable",
-		// 	2, ui.TableModelColumnAlwaysEditable, nil)
-		table.AppendCheckboxColumn("Check",
-			7, ui.TableModelColumnAlwaysEditable)
-		table.AppendButtonColumn("Delete",
-			6, ui.TableModelColumnAlwaysEditable)
-		input := ui.NewEntry()
-		button := ui.NewButton("Add")
-		// greeting := ui.NewLabel("")
-		box := ui.NewVerticalBox()
-		box.Append(ui.NewLabel("Enter your name:"), false)
-		box.Append(input, false)
-		box.Append(button, false)
-		box.Append(table, true)
-		// box.Append(greeting, false)
-		window := ui.NewWindow("IMSLP", 640, 480, true)
-		window.SetMargined(true)
-		window.SetChild(box)
-		button.OnClicked(func(*ui.Button) {
-			// title=append(title, input.Text())
-			// songURL, songName := search.Search(input.Text())
-			// greeting.SetText(songURL)
-			// greeting.SetText(songName)
-		})
-		window.OnClosing(func(*ui.Window) bool {
-			ui.Quit()
-			return true
-		})
-		window.Show()
-
-	})
-	if err != nil {
-		panic(err)
-	}
+	w := app.NewWindow("IMSLP Instrument Parsing") //window 선언
+	entry := widget.NewEntry()
+	entry.SetPlaceHolder("Tchaikovsky symphony no.5")
+	var data [][]string
+	hbox := widget.NewHBox(
+		entry, //엔트리 (문자열 입력)
+		widget.NewButton("Add", func() { //종료버튼
+			songURL, songName := search.Search(entry.Text)
+			log.Println(songURL)
+			log.Println(songName)
+			data = [][]string{{songName, songURL}}
+			// url = append(url, songURL)
+		}),
+	)
+	w.SetContent(
+		widget.NewVBox(
+			widget.NewLabel("IMSLP Instrument Parsing"), //레이블
+			widget.NewHBox(
+				entry, //엔트리 (문자열 입력)
+				widget.NewButton("Add", func() { //종료버튼
+					songURL, songName := search.Search(entry.Text)
+					log.Println(songURL)
+					log.Println(songName)
+					data = [][]string{{songName, songURL}}
+					widget.Refresh(hbox)
+					// url = append(url, songURL)
+				}),
+			),
+			makeTable( //makeTable함수를 사용하여 테이블 선언 및 변수 입력
+				[]string{"Title", "URL"}, //칼럼(헤더)
+				data,                     //데이터
+			),
+		),
+	)
+	w.Resize(fyne.NewSize(600, 600))
+	w.ShowAndRun()
 }
 
-type modelHandler struct {
-	row9Text    string
-	yellowRow   int
-	checkStates [15]int
+func makeTable(headings []string, rows [][]string) *widget.Box {
+
+	columns := rowsToColumns(headings, rows)
+
+	objects := make([]fyne.CanvasObject, len(columns))
+	for k, col := range columns {
+		box := widget.NewVBox(widget.NewLabelWithStyle(headings[k], fyne.TextAlignLeading, fyne.TextStyle{Bold: true}))
+		for _, val := range col {
+			box.Append(widget.NewLabel(val))
+		}
+		objects[k] = box
+	}
+	return widget.NewVBox(
+		fyne.NewContainerWithLayout(layout.NewGridLayout(len(columns)), objects...),
+	)
 }
 
-func newModelHandler() *modelHandler {
-	m := new(modelHandler)
-	m.row9Text = "You can edit this one"
-	m.yellowRow = -1
-	return m
+func rowsToColumns(headings []string, rows [][]string) [][]string {
+	columns := make([][]string, len(headings))
+	for _, row := range rows {
+		for colK := range row {
+			columns[colK] = append(columns[colK], row[colK])
+		}
+	}
+	return columns
 }
-
-func (mh *modelHandler) ColumnTypes(m *ui.TableModel) []ui.TableValue {
-	return []ui.TableValue{
-		ui.TableString(""), // column 0 text
-		ui.TableColor{},    // row background color
-		ui.TableColor{},    // column 1 text color
-		ui.TableImage{},    // column 1 image
-		ui.TableString(""), // column 4 button text
-		ui.TableInt(0),     // column 3 checkbox state
-	}
-}
-
-func (mh *modelHandler) NumRows(m *ui.TableModel) int {
-	return 15
-}
-
-var img [2]*ui.Image
-
-func (mh *modelHandler) CellValue(m *ui.TableModel, row, column int) ui.TableValue {
-	if column == 3 {
-		if row == mh.yellowRow {
-			return ui.TableColor{1, 1, 0, 1}
-		}
-		if row == 3 {
-			return ui.TableColor{1, 0, 0, 1}
-		}
-		if row == 11 {
-			return ui.TableColor{0, 0.5, 1, 0.5}
-		}
-		return nil
-	}
-	if column == 4 {
-		if (row % 2) == 1 {
-			return ui.TableColor{0.5, 0, 0.75, 1}
-		}
-		return nil
-	}
-	if column == 5 {
-		if row < 8 {
-			return ui.TableImage{img[0]}
-		}
-		return ui.TableImage{img[1]}
-	}
-	if column == 7 {
-		return ui.TableInt(mh.checkStates[row])
-	}
-	if column == 8 {
-		if row == 0 {
-			return ui.TableInt(0)
-		}
-		if row == 13 {
-			return ui.TableInt(100)
-		}
-		if row == 14 {
-			return ui.TableInt(-1)
-		}
-		return ui.TableInt(50)
-	}
-	switch column {
-	case 0:
-		return ui.TableString(fmt.Sprintf("Row %d", row))
-	case 2:
-		if row == 9 {
-			return ui.TableString(mh.row9Text)
-		}
-		return ui.TableString("Editing this won't change anything")
-	case 1:
-		return ui.TableString("Colors!")
-	case 6:
-		return ui.TableString("Make Yellow")
-	}
-	panic("unreachable")
-}
-
-func (mh *modelHandler) SetCellValue(m *ui.TableModel, row, column int, value ui.TableValue) {
-	if row == 9 && column == 2 {
-		mh.row9Text = string(value.(ui.TableString))
-	}
-	if column == 6 { // row background color
-		prevYellowRow := mh.yellowRow
-		mh.yellowRow = row
-		if prevYellowRow != -1 {
-			m.RowChanged(prevYellowRow)
-		}
-		m.RowChanged(mh.yellowRow)
-	}
-	if column == 7 { // checkboxes
-		mh.checkStates[row] = int(value.(ui.TableInt))
-	}
-}
-
-var rowData []string
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module imslp
 go 1.14
 
 require (
+	fyne.io/fyne v1.3.2
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/andlabs/ui v0.0.0-20200610043537-70a69d6ae31e
 	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da
+	github.com/inkyblackness/imgui-go v1.12.0 // indirect
 	github.com/jszwec/csvutil v1.3.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.0
 	github.com/rocketlaunchr/google-search v1.0.0
